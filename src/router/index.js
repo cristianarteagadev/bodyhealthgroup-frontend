ec9922d42fee7df0f7adab2cd3972a075474c58b
Cristian Arteaga <cristian.arteaga.dev@gmail.com>
2023-05-16 19:39:35 -0400

    Se configuro :pathMatch() para rutas no encontradas

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/CreatePost.vue')
+  },
+  {
+    path: '/:pathMatch(.*)',
+    name: '',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/NotFound.vue')
   }
 ]
 
